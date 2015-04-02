1586f9bedb5372bde0477dd0f57ea5a8e4e9b3ab
Hereward <hereward@planetonline.com.au>
2015-04-02 09:29:20 +0700

    added an additional test for recipe model

--- Test/models/recipe_modelTest.php
+++ Test/models/recipe_modelTest.php
@@ -25,6 +25,36 @@ class recipe_modelTest extends \PHPUnit_Framework_TestCase {
             array(array(array('item_0'=>67),array('item_1'=>24),array('item_2'=>83)),58)
         );
     }
+    
+    
+    public function testget_longevityReturnsAvalue() {
+        
+        $ingredients_model = $this->getMockBuilder('Ingredients_model')
+            ->setConstructorArgs(array())
+            ->getMock();
+        $ingredients_model->data = array(
+            array('bread', '10', 'slices', '4/4/2017'),
+            array('cheese', '250', 'slices', '25/12/2015'),
+            array('butter', '250', 'grams', '25/12/2015'),
+            array('peanut butter', 'grams', 'slices', '2/12/2015'),
+            array('mixed salad', '500', 'grams', '26/12/2015')
+            );
+        
+        $recipe_model = new Recipe_model();
+        $ingredient = array(
+            'item'=>'bread',
+            'amount'=>'2',
+            'unit'=>'slices'
+        );
+        
+        $result = $recipe_model->get_longevity($ingredient, $ingredients_model->data);
+        
+        $expectedResult = ($result>0);
+
+        $this->assertEquals($expectedResult, $result); 
+    }
+    
+   
      
 
 }
