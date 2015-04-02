13d299810e2d425c583f5c3f719e7340560148cf
Hereward <hereward@planetonline.com.au>
2015-04-02 07:43:09 +0700

    Added more tests

--- Test/models/ingredients_modelTest.php
+++ Test/models/ingredients_modelTest.php
@@ -1,39 +1,43 @@
 <?php
 
-require_once(__DIR__."../../../models/ingredients_model.php");
+require_once(__DIR__ . "../../../models/ingredients_model.php");
 
 class ingredients_modelTest extends \PHPUnit_Framework_TestCase {
-   
-    public function testparse_csv_returns_array() {
-        
-        $originalString = 'bread,10,slices,3/4/2015';
-        $container = array();
-        $container[0] = array('bread','10','slices','3/4/2015');
-        $expectedResult = $container;
-        
+    /**
+     * @dataProvider providerTestparse_csv_returns_array 
+     * @param string $originalString String to be sluggified
+     * @param string $expectedResult What we expect our slug result to be
+     */
+
+    public function testparse_csv_returns_array($originalString, $expectedResult) {
         $ingredients_model = new Ingredients_model();
-
         $result = $ingredients_model->parse_csv($originalString);
-
         $this->assertEquals($expectedResult, $result);
     }
-    
+
     public function testexpired_returns_true() {
-        $originaldata = array('bread','10','slices','3/4/2013');;
+        $originaldata = array('bread', '10', 'slices', '3/4/2013');
         $expectedResult = true;
         $ingredients_model = new Ingredients_model();
         $result = $ingredients_model->expired($originaldata);
         $this->assertEquals($expectedResult, $result);
-        
     }
-    
+
     public function testexpired_returns_false() {
-        $originaldata = array('bread','10','slices','3/4/2017');;
+        $originaldata = array('bread', '10', 'slices', '3/4/2017');
         $expectedResult = false;
         $ingredients_model = new Ingredients_model();
         $result = $ingredients_model->expired($originaldata);
         $this->assertEquals($expectedResult, $result);
-        
     }
+
+    public function providerTestparse_csv_returns_array() {
+        return array(
+            array('##,1/0,%%,13/4/2015', array(array('##', '1/0', '%%', '13/4/2015'))),
+            array('ONE,TWO,THREE,3/12/2015', array(array('ONE', 'TWO', 'THREE', '3/12/2015'))),
+            array('1', array(array('1')))
+        );
+    }
+    
 
 }
